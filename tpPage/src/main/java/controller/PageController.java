00e84b320f4060646df436af8cf935ca059ccb60
ds <ds@ds601-02>
2017-10-31 15:07:17 +0900

    gm

--- tpPage/src/main/java/controller/PageController.java
+++ tpPage/src/main/java/controller/PageController.java
@@ -7,10 +7,8 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import board.BoardService;
 import board.Mboard;
 import board.MboardCommand;
@@ -45,12 +43,14 @@ public class PageController {
 
 	@RequestMapping("/page")
 	public String pageLoad(@RequestParam("host") int host, @ModelAttribute("pboardcmd") PboardCommand pbc,
-			@ModelAttribute("mboardcmd") MboardCommand mbc, @ModelAttribute("logincmd") MemberCommand logincmd , Model model, HttpServletRequest request) {
+			@ModelAttribute("mboardcmd") MboardCommand mbc, @ModelAttribute("logincmd") MemberCommand logincmd,
+			Model model, HttpServletRequest request) {
 		int pageHostId = host;
 		Page page = pageDao.getPage(pageHostId);
 		if(page == null) {
 			return "redirect:/home"; 
 		} else {	
+			
 			int pbPage = boardSvc.pboardpage(pageHostId);
 			List<Pboard> pboardList = boardSvc.getPboardList(pageHostId);
 			int mbPage = boardSvc.mboardpage(pageHostId);
@@ -61,8 +61,13 @@ public class PageController {
 			request.setAttribute("mbPage", mbPage);
 			model.addAttribute("mboardList", mboardList);
 			
+			
+			
 			AuthInfo authInfo = (AuthInfo) request.getSession().getAttribute("authInfo");
+			
+			
 			if (authInfo !=null) {
+				System.out.println("11");
 				int memId = authInfo.getMid();
 				List<PageLike> ckList = pageDao.plikeCheck(memId, pageHostId);
 				if (ckList.size() == 0) {
@@ -71,37 +76,12 @@ public class PageController {
 					int ck = ckList.get(0).getPlike();
 					model.addAttribute("ck", ck);
 				} 
+				System.out.println("22");
 			}
+			
 			return "page";
 		}
 	}
-	
-	/*@RequestMapping("/page/{pid}")
-	public String pageLoadp(@PathVariable int pid, @ModelAttribute("pboardcmd") PboardCommand pbc, Model model, HttpServletRequest request) {
-		int pageHostId = pid;
-		int pbPage = boardSvc.pboardpage(pageHostId);
-		List<Pboard> pboardList = boardSvc.getPboardList(pageHostId);
-		int mbPage = boardSvc.mboardpage(pageHostId);
-		List<Mboard> mboardList = boardSvc.getMboardList(pageHostId);
-		request.setAttribute("pbPage", pbPage);
-		model.addAttribute("pboardList", pboardList);
-		request.setAttribute("mbPage", mbPage);
-		model.addAttribute("mboardList", mboardList);
-		
-		AuthInfo authInfo = (AuthInfo) request.getSession().getAttribute("authInfo");
-		if (authInfo !=null) {
-			int memId = authInfo.getMid();
-			int pageId = authInfo.getPid();
-			List<PageLike> ckList = pageDao.plikeCheck(memId, pageId);
-			if (ckList.size() == 0) {
-				model.addAttribute("ck", 0);
-			} else {
-				int ck = ckList.get(0).getPlike();
-				model.addAttribute("ck", ck);
-			} 
-		}
-		return "page";
-	}*/
 	
 	@RequestMapping("/pagemaker")
 	public String pageMaker(@ModelAttribute("pagecmd") PageCommand pmc, HttpServletRequest request) {
